c6853edfdde771cbb5ceaeff55bd1ef62b78cf07
Gisli Steinn Gislason <gislisg18@ru.is>
2020-09-03 10:07:51 +0000

    Asked for user input

--- verkefni1.py
+++ verkefni1.py
@@ -17,7 +17,8 @@ max_int = num_int
 while num_int >= 0:
     num_int = int(input("Input a number: "))
     if num_int > max_int:
-        max_int = num_int
 
-# Fill in the missing code
+
+
+
 print("The maximum is", max_int)    # Do not change this line
